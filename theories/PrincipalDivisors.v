6898983598f3a1afab71f2684b1e39b827889762
p1npo1nt <48339921+p1npo1nt@users.noreply.github.com>
2025-03-08 18:16:04 -0500

    .

--- theories/PrincipalDivisors.v
+++
@@ -1,9 +0,0 @@
-(* Definition of a principal divisor *)
-Definition ecdiv (f : {fraction ecring}) : divisor :=
-  fun p => order f p.
-
-Lemma deg_ecdiv_eq0 (f : {fraction ecring}) : 
-  sum (fun p => order f p) = 0.
-Proof.
-  (* Proof that the sum of orders is zero *)
-Admitted.
